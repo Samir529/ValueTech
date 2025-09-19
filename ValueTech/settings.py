3a9515628503a45414ba7e0cafcafe569c5904dc
Samir529 <samirasif529@gmail.com>
2025-09-19 13:30:19 +0600

    commit

--- ValueTech/settings.py
+++ ValueTech/settings.py
@@ -35,7 +35,8 @@ DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
-    'valuetech.onrender.com'
+    "valuetech.onrender.com",
+    "ValueTech.pythonanywhere.com"
 ]
 
 
@@ -43,6 +44,7 @@ CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:8000",
     "https://valuetech.onrender.com",
+    "https://valuetech.pythonanywhere.com/"
 ]
 
 
